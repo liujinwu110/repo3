23e5ac53780489a5bb100c47672d39f5c8923fab
liujinwu <573526823@qq.com>
2019-06-18 08:16:02 +0800

    提交ssm第3天

---
+++ parent/ssm-service/src/main/java/com/itheima/ssm/service/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.itheima.ssm.service.impl;
+
+import com.itheima.ssm.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("userService")
+public class UserServiceImpl implements UserService {
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
+}
